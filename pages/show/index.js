a897d566d8731453f8b33beaeba49478bf1d591d
unknown <unknown@example.com>
2021-03-07 10:22:00 +0800

    Initial Commit

---
+++ pages/show/index.js
@@ -0,0 +1,97 @@
+// pages/show/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    home:"返回主页",
+    array: ['中国', '美国', '英国', '日本'],
+    objectArray: [
+      {
+        id: 0,
+        name: '中国'
+      },
+      {
+        id: 1,
+        name: '美国'
+      },
+      {
+        id: 2,
+        name: '英国'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
+    index: 0,
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+show : function (){
+  wx.navigateTo({
+    url: '../index/index'
+  })
+},
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  
+},
+)
